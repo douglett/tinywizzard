36d970d518352491ef102efa516d73cf4e9c9dbc
Doug Inman <douglas.inman@live5.co.uk>
2025-01-04 01:22:24 +0000

    parsing and saving sexpression nodes

--- main.cpp
+++ main.cpp
@@ -1,10 +1,25 @@
 #include "tokenizer.hpp"
 #include <exception>
+#include <map>
 using namespace std;
+
+
+struct Sexpr {
+	enum SEXPR_TYPE {
+		T_NIL,
+		T_LIST,
+		T_TOKEN,
+		T_LITERAL
+	};
+	SEXPR_TYPE type;
+	string val;
+	vector<Sexpr> list;
+};
 
 
 struct AST : public TokenHelpers {
 	Tokenizer tok;
+	map<string, Sexpr> rules;
 
 	int parse() {
 		tok.flag_eol = 1;
@@ -12,45 +27,82 @@ struct AST : public TokenHelpers {
 		tok.show();
 		// parse lines
 		while (!tok.eof())
-			if (tok.peek() == "$EOL")  tok.get();
-			else if (!prule())  return false;
+			if      ( tok.peek() == "$EOL" )  tok.get();
+			else if ( !prule() )  return false;
+		show();
 		return true;
-	}
-
-	int error(const string& msg) {
-		// printf("error: parsing rule: %s (line %d)\n", msg.c_str(), tok.linepos());
-		throw runtime_error("error: " + msg
-			+ " (line " + to_string(tok.linepos()) + ")" );
-		return false;
 	}
 
 	int prule() {
 		string rulename;
-		if (!prulename(rulename))  return false;
-		if (!psexpr())  return false;
-		if ( tok.peek() != "$EOL" )  return error("expected $EOL after rule definition s-expression");
+		if ( !prulename(rulename) )  return false;
+		rules[rulename] = { Sexpr::T_LIST };  // define rule
+		if ( !psexpr( rules[rulename] ) )  return false;
+		if ( tok.peek() != "$EOL" )  return error("rule-definition", "expected $EOL after rule definition s-expression");
 		return true;
 	}
 
 	int prulename(string& rulename) {
-		// int pos = tok.pos;
 		if ( !isidentifier(tok.peek()) )  goto err;
 		rulename = tok.get();
 		if ( tok.peek() != ":" )  goto err;
 		tok.get();
 		if ( tok.peek() != "$EOL" )  goto err;
 		tok.get();
-		printf("rulename: [%s]\n", rulename.c_str());
+		// printf("rulename: [%s]\n", rulename.c_str());
 		return true;
 		// error
 		err:
-		// tok.pos = pos;
-		return error("expected 'rulename: $EOL'");
+		return error("rule-name", "expected 'rulename: $EOL'");
 	}
 
-	int psexpr() {
-		// err:
-		return error("error parsing bracketed s-expression");
+	int psexpr(Sexpr& sexpr) {
+		if ( tok.peek() != "(" )  goto err;
+		tok.get();
+		while (true)
+			if      ( tok.eof() )  goto err;
+			else if ( tok.peek() == "$EOL" )  tok.get();
+			else if ( tok.peek() == "(" )  sexpr.list.push_back({ Sexpr::T_LIST }), psexpr( sexpr.list.back() );
+			else if ( tok.peek() == ")" )  { tok.get();  break; }
+			else    sexpr.list.push_back({ Sexpr::T_TOKEN, tok.get() });
+		return true;
+		// error
+		err:
+		return error("s-expression", "error parsing bracketed s-expression");
+	}
+
+	int error(const string& rule, const string& msg) {
+		throw runtime_error(
+			rule + " error: " 
+			+ msg
+			+ " (line " + to_string(tok.linepos()) + ")" );
+		return false;
+	}
+
+	void show() {
+		for (const auto& rule : rules) {
+			cout << rule.first << ":";
+			showsexpr( rule.second, 1 );
+		}
+	}
+
+	void showsexpr(const Sexpr& sexpr, int indent=0) {
+		switch (sexpr.type) {
+			case Sexpr::T_NIL:      cout << "NIL ";  break;
+			case Sexpr::T_TOKEN:    cout << sexpr.val << " ";  break;
+			case Sexpr::T_LITERAL:  cout << sexpr.val << " ";  break;
+			case Sexpr::T_LIST:
+				cout << endl << string(indent*3, ' ') << "( ";
+				// for (auto& sx : sexpr.list)
+				// 	showsexpr( sx, indent+1 );
+				for (size_t i = 0; i < sexpr.list.size(); i++) {
+					showsexpr( sexpr.list[i], indent+1 );
+					if ( sexpr.list[i].type == Sexpr::T_LIST && i < sexpr.list.size()-1 && sexpr.list[i+1].type != Sexpr::T_LIST )
+						cout << endl << string((indent+1)*3, ' ');
+				}
+				cout << ") ";
+				break;
+		}
 	}
 };
 
