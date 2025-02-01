39ce762acf6c58279034aa80e35a671929b413f6
Doug Inman <douglas.inman@live5.co.uk>
2025-02-01 18:47:03 +0000

    code formatting

--- main.cpp
+++ main.cpp
@@ -8,29 +8,19 @@ using namespace std;
 
 struct Ruleset : TokenHelpers {
 	struct Rule { string type; vector<string>list; };
-	const vector<string> 
+	static inline const vector<string> 
 		RULE_TYPES      = { "accept", "require" },
 		RULE_PREDEF     = { "$eol" };
+	string name;
 	map<string, Rule> rules;
 
-	// int add(const string& name, const string& list) {
-	// 	return add( name, "accept", list );
-	// }
+	// === build ruleset ===
+
 	int add(const string& name, const string& list, const string& type = "accept") {
 		if ( rules.count(name) )
 			return error( "add", "duplicate rule name: " + name );
 		rules[ name ] = { type, splitstr(list) };
 		return true;
-	}
-
-	void show() {
-		for (const auto& [name, rule] : rules) {
-			printf("%s: (%s)\n    ", name.c_str(), rule.type.c_str());
-			for (auto& r : rule.list)
-				printf("%s ", r.c_str());
-			printf("\n");
-
-		} 
 	}
 
 	int validate() {
@@ -43,12 +33,9 @@ struct Ruleset : TokenHelpers {
 				return error( "validate", name + ": unknown rule type: " + rule.type );
 			// check individual rules exist
 			for (auto& subrule : rule.list)
-				if ( find( RULE_PREDEF.begin(), RULE_PREDEF.end(), subrule ) != RULE_PREDEF.end() )
-					continue;
-				else if ( rules.count( subrule ) )
-					continue;
-				else
-					return error( "validate", name + ": unknown subrule: " + subrule );
+				if      ( find( RULE_PREDEF.begin(), RULE_PREDEF.end(), subrule ) != RULE_PREDEF.end() ) ;
+				else if ( rules.count( subrule ) ) ;
+				else    return error( "validate", name + ": unknown subrule: " + subrule );
 		}
 		printf("ruleset validated!\n");
 		return true;
@@ -62,6 +49,21 @@ struct Ruleset : TokenHelpers {
 		return true;
 	}
 
+
+	//  === run ruleset ===
+
+
+	//  === helpers ===
+	void show() {
+		printf(":: %s ::\n", name.c_str());
+		for (const auto& [name, rule] : rules) {
+			printf("%s: (%s)\n    ", name.c_str(), rule.type.c_str());
+			for (auto& r : rule.list)
+				printf("%s ", r.c_str());
+			printf("\n");
+		} 
+	}
+
 	int error(const string& rule, const string& msg) {
 		throw runtime_error(
 			rule + " error: " 
@@ -71,7 +73,7 @@ struct Ruleset : TokenHelpers {
 };
 
 
-Ruleset testlang;
+Ruleset testlang = { {}, "testlang" };
 
 
 int main() {
