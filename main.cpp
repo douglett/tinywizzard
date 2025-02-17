2a665292341278ae5a6086967de35baf77076d6d
Doug Inman <douglas.inman@live5.co.uk>
2025-02-17 19:40:21 +0000

    made naming functions clearer

--- main.cpp
+++ main.cpp
@@ -28,10 +28,10 @@ struct Ruleset : TokenHelpers {
 	int validate() {
 		for (const auto& [name, rule] : rules) {
 			// check name
-			if ( !validatename(name) )
+			if ( !isvalidname(name) )
 				return error( "validate", name + ": bad rule name" );
 			// check rule type
-			if ( find( RULE_TYPES.begin(), RULE_TYPES.end(), rule.type ) == RULE_TYPES.end() )
+			if ( !isruletype(rule.type) )
 				return error( "validate", name + ": unknown rule type: " + rule.type );
 			// make sure the $program rule is defined
 			if ( !rules.count("$program") )
@@ -39,31 +39,40 @@ struct Ruleset : TokenHelpers {
 			// check individual rules exist
 			for (auto& rexstr : rule.list) {
 				auto rex = splitruleexpr(rexstr);
-				if      ( rex.name.length() && rex.name[0] != '$' ) ;
-				else if ( !isidentifier(rex.name.substr(1)) )
-					return error("validate", name + ": invalid rule name: " + rex.name);
-				else if ( find( RULE_PREDEF.begin(), RULE_PREDEF.end(), rex.name ) != RULE_PREDEF.end() ) ;
-				else if ( rules.count( rex.name ) ) ;
-				else    return error( "validate", name + ": unknown rule: " + rex.name );
+				if      ( ispredef( rex.name ) ) ;
+				else if ( isuserdef( rex.name ) ) ;
+				else    return error( "validate", name + ": unknown or invalid rule: " + rex.name );
 			}
 		}
 		printf("ruleset validated!\n");
 		return true;
 	}
 
-	int validatename(const string& name) {
+	int isvalidname(const string& name) {
 		if (name.length() < 2)  return false;
 		if (name[0] != '$')  return false;
 		for (size_t i = 1; i < name.length(); i++)
 			if ( !isalphanum(name[i]) )  return false;
 		return true;
 	}
+	int isruletype(const string& type) {
+		return find( RULE_TYPES.begin(), RULE_TYPES.end(), type ) != RULE_TYPES.end();
+	}
+	int isruleexpr(const string& ex) {
+		return find( RULE_EXPRESSIONS.begin(), RULE_EXPRESSIONS.end(), ex ) != RULE_EXPRESSIONS.end();
+	}
+	int ispredef(const string& name) {
+		return find( RULE_PREDEF.begin(), RULE_PREDEF.end(), name ) != RULE_PREDEF.end();
+	}
+	int isuserdef(const string& name) {
+		return rules.count( name );
+	}
 
 	RuleExpr splitruleexpr(string rexstr) {
 		RuleExpr rex = { "", 0, 0 };
 		if (rexstr.length() && rexstr.back() == '!')
 			rex.require = true, rexstr.pop_back();
-		if (rexstr.length() && find( RULE_EXPRESSIONS.begin(), RULE_EXPRESSIONS.end(), string()+rexstr.back() ) != RULE_EXPRESSIONS.end() )
+		if (rexstr.length() && isruleexpr( string()+rexstr.back() ) )
 			rex.expr = rexstr.back(), rexstr.pop_back();
 		rex.name = rexstr;
 		return rex;
@@ -94,46 +103,54 @@ struct Parser : TokenHelpers {
 
 	int parse(const string& fname) {
 		// tokenize
+		printf("-----\n");
 		printf("loading file: %s\n", fname.c_str());
 		if ( !tok.tokenize(fname) )
 			error("parse", tok.errormsg);
 		tok.show();
 		// parse program
-		if ( !prule("$program") )
+		if ( !pruleexpr("$program") )
 			return error( "$program", "unknown error parsing $program" );
 		printf("file parsed successfully!");
 		// ok
 		return true;
 	}
 
-	int prule(const string& rulename) {
-		printf("parsing rule: %s %s\n", rulename.c_str(), tok.peek().c_str());
-		// built in rules
-		if ( rulename == "$eof" ) {
-			if (!tok.eof())  return false;
-		}
-		else if ( rulename == "$eol" ) {
-			if (tok.peek() != "$EOL")  return false;
-			tok.get();
-		}
-		else if ( rulename == "$identifier" ) {
-			if (!isidentifier( tok.peek() ))  return false;
-			tok.get();
-		}
-		// user defined rules
-		else if ( rulename.size() && rulename[0] == '$' ) {
-			if ( !ruleset.rules.count(rulename) )
-				return error( "prule", "missing rule: " + rulename );
-			for (const auto& subrule : ruleset.rules[rulename].list)
-				if (!prule( subrule ))  return false;
-		}
-		// match text
-		else {
-			if ( tok.peek() != rulename )  return false;
-			tok.get();
-		}
-		return true;
+	int pruleexpr(const string& ruleexpr) {
+		printf("parsing RuleExpr: '%s' @ '%s'\n", ruleexpr.c_str(), tok.peek().c_str());
+		auto rex = ruleset.splitruleexpr(ruleexpr);
+
+		return false;
 	}
+
+	// int prule(const string& rulename) {
+	// 	printf("parsing rule: %s %s\n", rulename.c_str(), tok.peek().c_str());
+	// 	// built in rules
+	// 	if ( rulename == "$eof" ) {
+	// 		if (!tok.eof())  return false;
+	// 	}
+	// 	else if ( rulename == "$eol" ) {
+	// 		if (tok.peek() != "$EOL")  return false;
+	// 		tok.get();
+	// 	}
+	// 	else if ( rulename == "$identifier" ) {
+	// 		if (!isidentifier( tok.peek() ))  return false;
+	// 		tok.get();
+	// 	}
+	// 	// user defined rules
+	// 	else if ( rulename.size() && rulename[0] == '$' ) {
+	// 		if ( !ruleset.rules.count(rulename) )
+	// 			return error( "prule", "missing rule: " + rulename );
+	// 		for (const auto& subrule : ruleset.rules[rulename].list)
+	// 			if (!prule( subrule ))  return false;
+	// 	}
+	// 	// match text
+	// 	else {
+	// 		if ( tok.peek() != rulename )  return false;
+	// 		tok.get();
+	// 	}
+	// 	return true;
+	// }
 
 	int error(const string& rule, const string& msg) {
 		throw runtime_error( rule + " error: " + msg );
@@ -150,8 +167,8 @@ struct TestlangParser : Parser {
 
 		// initialise ruleset
 		ruleset.name = "testlang";
-		ruleset.add( "$program", "$line* $eof" );
-		ruleset.add( "$line", "print $identifier $eol" );
+		ruleset.add( "$program", "$identifier* $eof" );
+		// ruleset.add( "$line", "$identifier* $eol" );
 		ruleset.show();
 		ruleset.validate();
 		return true;
@@ -164,5 +181,5 @@ int main() {
 
 	TestlangParser parser;
 	parser.init();
-	// parser.parse("test/test1.script");
+	parser.parse("test/test1.script");
 }
