98194757aef461bb71a47800755976edc75e235c
Doug Inman <douglas.inman@live5.co.uk>
2025-04-15 18:00:56 +0100

    cleanup of compiled output

--- compiler.hpp
+++ compiler.hpp
@@ -13,20 +13,9 @@ struct Compiler : TokenHelpers {
 	};
 	struct Instruction { INSTRUCTION_TYPE type; vector<string> args; };
 	vector<Instruction> inheader, inprogram;
-
-	stringstream header, output;
-	vector<string> literals;
 	int ifcount = 0, litcount = 0;
 
 	int init() {
-		// header.str(""), header.clear();
-		// output.str(""), output.clear();
-		// literals = {};
-		// ifcount = 0;
-		// header << "# control variables\n";
-		// header << "	dim CONTROL TEMP\n";
-		// header << "	dim A B C D E F G H I J K L M N O P Q R S T U V W X Y Z\n";
-
 		ifcount = litcount = 0;
 		inheader = inprogram = {};
 		inheader.push_back({ IN_NOOP, { "# control variables" }});
@@ -42,11 +31,9 @@ struct Compiler : TokenHelpers {
 		auto& type = json.at("type").str;
 
 		if (json.count("dsym")) {
-			// output << "# dsym: line " << json.at("dsym").num << "\n";
 			inprogram.push_back({ IN_DSYM, { to_string((int)json.at("dsym").num) } });
 		}
 		if (json.count("linenumber")) {
-			// output << "BASIC_LINE_" << json.at("linenumber").num << ":\n";
 			inprogram.push_back({ IN_LABEL, { "BASIC_LINE_" + to_string((int)json.at("linenumber").num) } });
 		}
 
@@ -56,25 +43,22 @@ struct Compiler : TokenHelpers {
 		}
 		// basic commands
 		else if (type == "$end") {
-			// output << "	end\n";
 			inprogram.push_back({ IN_END });
 		}
 		else if (type == "$goto") {
-			// output << "	goto BASIC_LINE_" << json.at("value").num << "\n";
 			inprogram.push_back({ IN_JUMP, { "BASIC_LINE_" + to_string((int)json.at("value").num) } });
 		}
 		// I/O
 		else if (type == "$print") {
 			for (auto& printval : json.at("value").arr) {
+				// print string literal data
 				if (printval.at("type").str == "$stringliteral") {
-					// literals.push_back(printval.at("value").str);
-					// output << "	prints STRING_LIT_" << (literals.size() - 1) << "\n";
 					string strid = "STRING_LIT_" + to_string(++litcount);
 					inheader.push_back({ IN_DATA, { strid, printval.at("value").str } });
 					inprogram.push_back({ IN_PRINTS, { strid } });
 				}
+				// print variable as number
 				else if (printval.at("type").str == "$variable") {
-					// output << "	printi " << printval.at("value").str << "\n";
 					inprogram.push_back({ IN_PRINTV, { printval.at("value").str } });
 				}
 				else
@@ -83,28 +67,20 @@ struct Compiler : TokenHelpers {
 		}
 		else if (type == "$input") {
 			assert(json.at("value").size() == 1);  // check for multiple inputs
-			// output << "	input " << json.at("value").at(0).at("value").str << "\n";
 			inprogram.push_back({ IN_INPUT, { json.at("value").at(0).at("value").str } });
 		}
 		// control structures
 		else if (type == "$if") {
 			string ifid = "BASIC_POSTIF_" + to_string(++ifcount);
 			compile(json.at("value").at(0));
-			// output << "	goto_ifn BASIC_POSTIF_" << ifid << "\n";
 			inprogram.push_back({ IN_JUMPIFN, { ifid } });
 			compile(json.at("value").at(1));
-			// output << "BASIC_POSTIF_" << ifid << ":\n";
 			inprogram.push_back({ IN_LABEL, { ifid } });
 		}
 		else if (type == "$comparison") {
 			compile(json.at("value").at(0));
 			compile(json.at("value").at(1));
 			auto op = json.at("operator").str;
-			// if      (op == "<" )  output << "	compare_lt\n";
-			// else if (op == ">" )  output << "	compare_gt\n";
-			// else if (op == "<=")  output << "	compare_lte\n";
-			// else if (op == ">=")  output << "	compare_gte\n";
-			// else    error(type, "unknown operator: " + op);
 			if      (op == "<" )  inprogram.push_back({ IN_LT });
 			else if (op == ">" )  inprogram.push_back({ IN_GT });
 			else if (op == "<=")  inprogram.push_back({ IN_LTE });
@@ -114,15 +90,12 @@ struct Compiler : TokenHelpers {
 		// expressions
 		else if (type == "$let") {
 			compile(json.at("value").at(1));  // handle expression
-			// output << "	put " << json.at("value").at(0).at("value").str << "\n";  // put in memory
 			inprogram.push_back({ IN_PUT, { json.at("value").at(0).at("value").str } });  // put in memory
 		}
 		else if (type == "$variable") {
-			// output << "	get " << json.at("value").str << "\n";  // get from memory
 			inprogram.push_back({ IN_GET, { json.at("value").str } });  // get from memory
 		}
 		else if (type == "$integer") {
-			// output << "	push " << json.at("value").num << "\n";
 			inprogram.push_back({ IN_PUSH, { to_string((int)json.at("value").num) } });
 		}
 		else if (type == "$add" || type == "$mul") {
@@ -131,11 +104,6 @@ struct Compiler : TokenHelpers {
 			for (int i = 1; i < value.size(); i++) {
 				compile(value.at(i).at("value"));
 				auto op = value.at(i).at("operator").str;
-				// if      (op == "+")  output << "	add\n";
-				// else if (op == "-")  output << "	sub\n";
-				// else if (op == "*")  output << "	mul\n";
-				// else if (op == "/")  output << "	div\n";
-				// else    error(type, "unknown operator: " + op);
 				if      (op == "+")  inprogram.push_back({ IN_ADD });
 				else if (op == "-")  inprogram.push_back({ IN_SUB });
 				else if (op == "*")  inprogram.push_back({ IN_MUL });
@@ -149,15 +117,6 @@ struct Compiler : TokenHelpers {
 		return true;
 	}
 
-	// void show() {
-	// 	fstream fs("output.asm", ios::out);
-	// 	fs << header.str();
-	// 	fs << "# define literals\n";
-	// 	for (size_t i = 0; i < literals.size(); i++)
-	// 		fs << "	data STRING_LIT_" << i << " " << literals[i] << "\n";
-	// 	fs << output.str();
-	// }
-
 	void show() {
 		fstream fs("output2.asm", ios::out);
 		for (const auto& in : inheader)
@@ -169,9 +128,11 @@ struct Compiler : TokenHelpers {
 	string showinstruction(const Instruction& in) {
 		string s = "\t";
 		switch (in.type) {
+			// pseudo-instructions
 			case IN_NOOP:      s = in.args.size() ? in.args.at(0) : "";  break;
 			case IN_DSYM:      s = "# dsym: line " + in.args.at(0);  break;
 			case IN_LABEL:     s = in.args.at(0) + ":";  break;
+			// data
 			case IN_DIM:
 				s += "dim ";
 				for (const auto& arg : in.args)
@@ -182,6 +143,7 @@ struct Compiler : TokenHelpers {
 				for (const auto& arg : in.args)
 					s += arg + " ";
 				break;
+			// control
 			case IN_END:       s += "end";  break;
 			case IN_JUMP:      s += "jump " + in.args.at(0);  break;
 			case IN_PRINTS:    s += "prints " + in.args.at(0);  break;
@@ -190,6 +152,7 @@ struct Compiler : TokenHelpers {
 			case IN_PUT:       s += "put " + in.args.at(0);  break;
 			case IN_GET:       s += "get " + in.args.at(0);  break;
 			case IN_PUSH:      s += "push " + in.args.at(0);  break;
+			// maths
 			case IN_ADD:       s += "add";  break;
 			case IN_SUB:       s += "sub";  break;
 			case IN_MUL:       s += "mul";  break;
@@ -205,7 +168,6 @@ struct Compiler : TokenHelpers {
 	}
 
 	int error(const string& type, const string& msg) {
-		// throw runtime_error("compiler error in " + type + ": " + msg);
 		cout << "compiler error in " + type + ": " + msg << endl;
 		return false;
 	}
