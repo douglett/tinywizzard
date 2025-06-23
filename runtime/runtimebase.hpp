7f52f7b6d9b8c6ed8b4550bb0533754b96541560
Doug Inman <doug@r3spin.co.uk>
2025-06-23 12:32:54 +0100

    code spacing

--- runtime/runtimebase.hpp
+++ runtime/runtimebase.hpp
@@ -25,9 +25,9 @@ struct RuntimeBase {
 		string s = "\t";
 		switch (in.type) {
 			// pseudo-instructions
-			case IN_NOOP:      s = "# " + (in.args.size() ? in.args.at(0) : "");  break;
-			case IN_DSYM:      s = "# dsym: line " + to_string(in.argi);  break;
-			case IN_LABEL:     s = in.args.at(0) + ":";  break;
+			case IN_NOOP:        s = "# " + (in.args.size() ? in.args.at(0) : "");  break;
+			case IN_DSYM:        s = "# dsym: line " + to_string(in.argi);  break;
+			case IN_LABEL:       s = in.args.at(0) + ":";  break;
 			// data
 			case IN_DIM:
 				s += "dim ";
@@ -39,35 +39,35 @@ struct RuntimeBase {
 					+ in.args.at(0) 
 					+ " \"" + TokenHelpers::escapeliteral(in.args.at(1)) + "\"";
 				break;
-			case IN_MAKESTR:   s += "makestr";  break;
-			case IN_COPYSTR:   s += "copystr";  break;
-			case IN_EQUALSSTR: s += "equalsstr";  break;
+			case IN_MAKESTR:     s += "makestr";  break;
+			case IN_COPYSTR:     s += "copystr";  break;
+			case IN_EQUALSSTR:   s += "equalsstr";  break;
 			// control
-			case IN_END:       s += "end";  break;
-			case IN_JUMP:      s += "jump " + in.args.at(0);  break;
-			case IN_CALL:      s += "call " + in.args.at(0);  break;
-			case IN_RETURN:    s += "return";  break;
-			case IN_INPUT:     s += "input " + in.args.at(0);  break;
-			case IN_INPUTI:    s += "inputi " + in.args.at(0);  break;
-			case IN_PUT:       s += "put " + in.args.at(0);  break;
-			case IN_GET:       s += "get " + in.args.at(0);  break;
-			case IN_PUSH:      s += "push " + to_string(in.argi);  break;
-			case IN_PRINTI:    s += "printi";  break;
-			case IN_PRINTC:    s += "printc";  break;
-			case IN_PRINTS:    s += "prints";  break;
+			case IN_END:         s += "end";  break;
+			case IN_JUMP:        s += "jump " + in.args.at(0);  break;
+			case IN_CALL:        s += "call " + in.args.at(0);  break;
+			case IN_RETURN:      s += "return";  break;
+			case IN_INPUT:       s += "input " + in.args.at(0);  break;
+			case IN_INPUTI:      s += "inputi " + in.args.at(0);  break;
+			case IN_PUT:         s += "put " + in.args.at(0);  break;
+			case IN_GET:         s += "get " + in.args.at(0);  break;
+			case IN_PUSH:        s += "push " + to_string(in.argi);  break;
+			case IN_PRINTI:      s += "printi";  break;
+			case IN_PRINTC:      s += "printc";  break;
+			case IN_PRINTS:      s += "prints";  break;
 			// maths
-			case IN_ADD:       s += "add";  break;
-			case IN_SUB:       s += "sub";  break;
-			case IN_MUL:       s += "mul";  break;
-			case IN_DIV:       s += "div";  break;
-			case IN_EQ:        s += "compare_eq";  break;
-			case IN_NEQ:       s += "compare_neq";  break;
-			case IN_LT:        s += "compare_lt";  break;
-			case IN_GT:        s += "compare_gt";  break;
-			case IN_LTE:       s += "compare_lte";  break;
-			case IN_GTE:       s += "compare_gte";  break;
-			case IN_JUMPIF:    s += "jumpif "  + in.args.at(0);  break;
-			case IN_JUMPIFN:   s += "jumpifn " + in.args.at(0);  break;
+			case IN_ADD:         s += "add";  break;
+			case IN_SUB:         s += "sub";  break;
+			case IN_MUL:         s += "mul";  break;
+			case IN_DIV:         s += "div";  break;
+			case IN_EQ:          s += "compare_eq";  break;
+			case IN_NEQ:         s += "compare_neq";  break;
+			case IN_LT:          s += "compare_lt";  break;
+			case IN_GT:          s += "compare_gt";  break;
+			case IN_LTE:         s += "compare_lte";  break;
+			case IN_GTE:         s += "compare_gte";  break;
+			case IN_JUMPIF:      s += "jumpif "  + in.args.at(0);  break;
+			case IN_JUMPIFN:     s += "jumpifn " + in.args.at(0);  break;
 		}
 		return s;
 	}
